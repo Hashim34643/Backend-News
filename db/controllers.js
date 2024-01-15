f34b1e89e02ca9ef10d2222a8ca1d733457367f5
Hashim34643 <hashimhussain7312@gmail.com>
2024-01-15 14:07:03 +0000

    task 2 completed

--- db/controllers.js
+++ db/controllers.js
@@ -1,4 +1,5 @@
 const models = require("/home/hashim/northcoders/backend/be-nc-news/db/models.js");
+const fs = require("fs/promises");
 
 function getAllTopics(req, res) {
     models.fetchAllTopics().then((response) => {
@@ -9,4 +10,25 @@ function getAllTopics(req, res) {
     })
 }
 
-module.exports = {getAllTopics};
+function updateEndpointsDynamically(data) {
+    return fs.writeFile("/home/hashim/northcoders/backend/be-nc-news/endpoints.json", JSON.stringify(data, null, 2), "utf-8");
+}
+
+function getAllApi(req, res) {
+    fs.readFile("/home/hashim/northcoders/backend/be-nc-news/endpoints.json", "utf-8").then((response) => {
+        const endpoints = JSON.parse(response);
+        return models.fetchAllApi().then((response) => {
+            const newEndpoints = {
+                ...endpoints,
+                ...response,
+            }
+            return updateEndpointsDynamically(newEndpoints).then((response) => {
+                res.status(200).send({newEndpoints});
+            })
+        })
+    }).catch((error) => {
+        console.error("error reading file getAllApi Func", error);
+    })
+}
+
+module.exports = {getAllTopics, getAllApi};
